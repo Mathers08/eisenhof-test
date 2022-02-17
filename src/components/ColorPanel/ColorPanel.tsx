26216de34dee086dc54738b6b7d6e7c8bd632e7d
mathers08 <black.mathers08@mail.ru>
2022-02-17 21:05:17 +0300

    damn...

--- src/components/ColorPanel/ColorPanel.tsx
+++ src/components/ColorPanel/ColorPanel.tsx
@@ -1,4 +1,7 @@
 import styles from './ColorPanel.module.scss';
+import Slider from "react-slick";
+import {FaArrowLeft, FaArrowRight} from "react-icons/fa";
+import {useState} from "react";
 
 interface ColorPanelProps {
   colorNames: string[],
@@ -28,17 +31,45 @@ const ColorPanel = ({colorNames, changeCircleColor}: ColorPanelProps) => {
       'right': '#771b1c'
     },
   ];
+  const [circleIndex, setCircleIndex] = useState(0);
+  const PrevArrow = ({onClick}: any) => {
+    return (
+      <div className={`${styles.arrow} ${styles.prev}`} onClick={onClick}>
+        <FaArrowLeft/>
+      </div>
+    )
+  }
+  const NextArrow = ({onClick}: any) => {
+    return (
+      <div className={`${styles.arrow} ${styles.next}`} onClick={onClick}>
+        <FaArrowRight/>
+      </div>
+    )
+  }
+  const settings = {
+    infinite: true,
+    speed: 300,
+    centerMode: true,
+    centerPadding: '0',
+    nextArrow: <NextArrow/>,
+    prevArrow: <PrevArrow/>,
+    beforeChange: (current: number, next: number) => setCircleIndex(next)
+  }
 
   return (
-    <div className={styles.footer}>
-      {colors.map((color, index) => (
-        <div
-          key={color.left}
-          className={styles.circle}
-          style={{background: `linear-gradient(to left, ${color.left} 50%, ${color.right} 50%)`}}
-          onClick={() => changeCircleColor(colorNames[index])}
-        />
-      ))}
+    <div className={styles.colorPanel}>
+      <Slider className={styles.slider} {...settings}>
+        {colors.map((color, index) => (
+          <div className={index === circleIndex ? `${styles.slide} ${styles.active}` : `${styles.slide}`}>
+            <div
+              key={color.left}
+              className={styles.circle}
+              style={{background: `linear-gradient(to left, ${color.left} 50%, ${color.right} 50%)`}}
+              onClick={() => changeCircleColor(colorNames[index])}
+            />
+          </div>
+        ))}
+      </Slider>
     </div>
   );
 };
